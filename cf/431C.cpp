dd3c217a05d429bde1932ed9674c3e85a734c2df
jlgm <jlgm@cin.ufpe.br>
2017-01-12 08:41:25 -0300

    readding 431C - dp for counting paths on k-tree (one less dimension)

--- cf/431C.cpp
+++ cf/431C.cpp
@@ -18,18 +18,18 @@ typedef pair<int, int> pii;
 template <class _T> inline string tostr(const _T& a) { ostringstream os(""); os << a; return os.str(); }
 
 ll n, d, k;
-ll dp[105][105][2];
+ll dp[105][2];
 
-ll calc(ll N, ll K, ll D) {
+ll calc(ll N, ll D) {
     if (N == 0 && D) return 1;
     if (N < 0) return 0;
-    if (dp[N][K][D] != -1) return dp[N][K][D];
+    if (dp[N][D] != -1) return dp[N][D];
     int ret = 0;
-    fr(i,1,K+1) {
-        ret += (calc(N-i,K,(i>=d)||D)%MOD);
+    fr(i,1,k+1) {
+        ret += (calc(N-i,(i>=d)||D)%MOD);
         ret%=MOD;
     }
-    return dp[N][K][D] = ret%MOD;
+    return dp[N][D] = ret%MOD;
 }
 
 int main() {
@@ -37,7 +37,7 @@ int main() {
     scanf("%lld%lld%lld", &n, &k, &d);
     ms(dp,-1);
 
-    printf("%lld\n", calc(n,k,0));
+    printf("%lld\n", calc(n,0));
 
     return 0;
 }
