1a7cf0b82094b0df2cfb89f2cdeea4c1b4f5f4eb
jlgm <jlgm@cin.ufpe.br>
2017-03-01 06:03:43 -0300

    adding 221A

---
+++ cf/221A.cpp
@@ -0,0 +1,27 @@
+#pragma comment(linker, "/STACK:16777216")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ms(ar,a) memset(ar, a, sizeof(ar))
+#define fr(i,j,k) for (int (i) = (j); (i) < (k); (i)++)
+#define rf(i,j,k) for (int (i) = (j); (i) >= (k); (i)--)
+#define db(x) cout << (#x) << " = " << x << endl;
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int n;
+
+int main() {
+
+    scanf("%d", &n);
+    printf("%d", n);
+    fr(i,1,n) printf(" %d", i);
+
+    return 0;
+}
