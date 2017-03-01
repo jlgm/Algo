2cde6495be38f62a74d8262f41458d2c43cc7653
jlgm <jlgm@cin.ufpe.br>
2017-03-01 06:16:02 -0300

    sending BIT to lib

--- lib/fenwick-tree.cpp
+++ lib/fenwick-tree.cpp
@@ -15,45 +15,30 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-int n, a[1000006], dp[1000006], pd[1000006], f[1000006];
-unordered_map<int,int> h, g;
+int n, a[100005], f[100005];
 
-void update(int p, int val) {
-    for (int i = p; i <= n; i += i&-i)
-        f[i] += val;
+void update(int id, int v) {
+    while(id <= n) f[id] += v, id += (id & -id);
 }
 
-int sum(int p) {
+int sum(int id) {
     int ret = 0;
-    for (int i = p; i; i -= i&-i)
-        ret += f[i];
+    while(id > 0) ret += f[id], id -= (id & -id);
     return ret;
 }
 
 int main() {
 
-    /* usage in codeforces 459D */
-    /* http://codeforces.com/contest/459/problem/D */
-
     scanf("%d", &n);
     fr(i,0,n) scanf("%d", &a[i]);
 
-    fr(i,0,n) {
-        dp[i] = h[a[i]] + 1;
-        h[a[i]]++;
-    }
+    int ans = 0;
     rf(i,n-1,0) {
-        pd[i] = g[a[i]] + 1;
-        g[a[i]]++;
+        ans += sum(a[i]-1); //counts how many inversions array a has
+        update(a[i], 1);
     }
 
-    ll ans = 0;
-    rf(i,n-1,0) { //counts how many pairs (i < j) there are such that dp(i) > pd(j) (uses BIT)
-        ans += (ll)sum(dp[i]);
-        update(pd[i]+1, 1);
-    }
-
-    printf("%lld\n", ans);
+    printf("%d\n", ans);
 
     return 0;
 }
