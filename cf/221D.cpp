fcfb40c44f65189e7fcc5ba575f003bfea53679d
jlgm <jlgm@cin.ufpe.br>
2017-03-01 09:01:06 -0300

    adding 221D using mo's algorithm

--- cf/221D.cpp
+++ cf/221D.cpp
@@ -17,74 +17,70 @@ typedef pair<int, int> pii;
 
 #define N 111111
 #define A 1111111
-#define BLOCK 555 // ~sqrt(N)
+#define BLOCK 330 // ~sqrt(N)
 
 int a[N], ans[N], answer = 0;
 int cnt[A];
 
 struct node {
-	int L, R, i;
-}q[N];
+    int L, R, i;
+} q[N];
 
 bool cmp(node x, node y) {
-	if(x.L/BLOCK != y.L/BLOCK) {
-		// different blocks, so sort by block.
-		return x.L/BLOCK < y.L/BLOCK;
-	}
-	// same block, so sort by R value
-	return x.R < y.R;
+    if(x.L/BLOCK != y.L/BLOCK) {
+        return x.L/BLOCK < y.L/BLOCK;
+    }
+    return x.R < y.R;
 }
 
 void remove(int position) {
     if (a[position] > A) return;
     if(cnt[a[position]] == a[position]) answer--;
-	cnt[a[position]]--;
-	if(cnt[a[position]] == a[position]) answer++;
+    cnt[a[position]]--;
+    if(cnt[a[position]] == a[position]) answer++;
 }
 
 void add(int position) {
     if (a[position] > A) return;
     if(cnt[a[position]] == a[position]) answer--;
-	cnt[a[position]]++;
-	if(cnt[a[position]] == a[position]) answer++;
+    cnt[a[position]]++;
+    if(cnt[a[position]] == a[position]) answer++;
 }
 
 int main() {
-	int n,m;
-	scanf("%d%d", &n, &m);
-	for(int i=0; i<n; i++)
-		scanf("%d", &a[i]);
+    int n,m;
+    scanf("%d%d", &n, &m);
+    fr(i,0,n) scanf("%d", &a[i]);
 
-	for(int i=0; i<m; i++) {
-		scanf("%d%d", &q[i].L, &q[i].R);
-		q[i].L--; q[i].R--;
-		q[i].i = i;
-	}
+    fr(i,0,m) {
+        scanf("%d%d", &q[i].L, &q[i].R);
+        q[i].L--; q[i].R--;
+        q[i].i = i;
+    }
 
-	sort(q, q + m, cmp);
+    sort(q, q + m, cmp);
 
-	int currentL = 0, currentR = 0;
-	for(int i=0; i<m; i++) {
-		int L = q[i].L, R = q[i].R;
-		while(currentL < L) {
-			remove(currentL);
-			currentL++;
-		}
-		while(currentL > L) {
-			add(currentL-1);
-			currentL--;
-		}
-		while(currentR <= R) {
-			add(currentR);
-			currentR++;
-		}
-		while(currentR > R+1) {
-			remove(currentR-1);
-			currentR--;
-		}
-		ans[q[i].i] = max(answer,0);
-	}
+    int currentL = 0, currentR = 0;
+    for(int i=0; i<m; i++) {
+        int L = q[i].L, R = q[i].R;
+        while(currentL < L) {
+            remove(currentL);
+            currentL++;
+        }
+        while(currentL > L) {
+            add(currentL-1);
+            currentL--;
+    	}
+        while(currentR <= R) {
+            add(currentR);
+            currentR++;
+        }
+        while(currentR > R+1) {
+            remove(currentR-1);
+            currentR--;
+        }
+        ans[q[i].i] = max(answer,0);
+    }
 
-	for(int i=0; i<m; i++)
-		printf("%d\n", ans[i]);
+    fr(i,0,m) printf("%d\n", ans[i]);
 }
