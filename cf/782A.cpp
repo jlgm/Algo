ffba425ae140265479ce19fa8b103b978d2c4b8e
jlgm <jlgm@cin.ufpe.br>
2017-03-05 12:50:10 -0300

    adding 782A

---
+++ cf/782A.cpp
@@ -0,0 +1,36 @@
+#pragma comment(linker, "/STACK:16777216")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ms(ar,a) memset(ar, a, sizeof(ar))
+#define fr(i,j,k) for (int (i) = (j); (i) < (k); (i)++)
+#define rf(i,j,k) for (int (i) = (j); (i) >= (k); (i)--)
+#define db(x) cout << (#x) << " = " << x << endl;
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int n, p;
+bool mark[100005];
+
+int main() {
+
+    scanf("%d", &n);
+    int ans = 0, cur = 0;
+    fr(i,0,(n*2)) {
+        scanf("%d", &p);
+        if (!mark[p]) cur++;
+        else cur--;
+        mark[p] = true;
+        ans = max(ans,cur);
+    }
+
+    printf("%d\n", ans);
+
+    return 0;
+}
