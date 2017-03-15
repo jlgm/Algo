c260179341c3f47b94445bd3471480b7bdd25981
jlgm <jlgm@cin.ufpe.br>
2017-03-15 15:11:19 -0300

    adding 785A from live contest

---
+++ cf/785A.cpp
@@ -0,0 +1,37 @@
+#pragma comment(linker, "/STACK:16777216")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ms(ar,a) memset(ar, a, sizeof(ar))
+#define fr(i,j,k) for (int (i) = (j); (i) < (k); (i)++)
+#define rf(i,j,k) for (int (i) = (j); (i) >= (k); (i)--)
+#define db(x) cout << (#x) << " = " << x << endl;
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+map<string,int> m;
+int n;
+
+int main() {
+
+    scanf("%d", &n);
+    string s; int ans = 0;
+
+    m["Tetrahedron"] = 4;
+    m["Cube"] = 6;
+    m["Octahedron"] = 8;
+    m["Dodecahedron"] = 12;
+    m["Icosahedron"] = 20;
+
+    fr(i,0,n) cin >> s, ans += m[s];
+
+    printf("%d\n", ans);
+
+    return 0;
+}
